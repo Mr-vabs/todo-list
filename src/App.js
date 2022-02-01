663cb993533f447e3b691f85e19d05fb355d0630
Vaibhav <33443357+Mr-vabs@users.noreply.github.com>
2022-02-01 16:58:10 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          start to build your own react app, vaibhav
+          Start to build your own react app, Vaibhav
         </p>
         <a
           className="App-link"
